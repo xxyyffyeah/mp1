f62067cdcf955db807e2d5356b3e370b5f23da2c
xxyyffyeah <xxyyffyeah@126.com>
2025-09-13 16:35:39 -0500

    1-6

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,36 @@
 /* Your JS here. */
-console.log('Hello World!')
+// console.log('Hello World!')
+const navLinks = document.querySelectorAll('#header nav a');
+const sections = document.querySelectorAll('main section');
+const header = document.querySelector('#header');
+const headerHeight = header.offsetHeight;
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    const currentScroll = window.pageYOffset;
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        //console.log('Section Top:', sectionTop, 'Current Scroll:', currentScroll, 'Header Height:', headerHeight);
+        if (currentScroll + headerHeight >= sectionTop && currentScroll + headerHeight < sectionTop + section.offsetHeight) {
+            current = section.getAttribute('id');
+        }
+    })
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${current}`) {
+            link.classList.add('active');
+        }
+    })
+    if(currentScroll > 50){
+        header.classList.remove('onTop');
+        navLinks.forEach(link => {
+            link.classList.remove('onTop');
+        });
+    }
+    else{
+        header.classList.add('onTop');
+        navLinks.forEach(link => {
+            link.classList.add('onTop');
+        }); 
+    }
+});
